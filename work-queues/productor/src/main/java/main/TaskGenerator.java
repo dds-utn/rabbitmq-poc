58d92f981d7ece6ef02870ae8e4894d02e7e205e
Ezequiel Escobar <ezequieloscarescobar2@gmail.com>
2024-11-04 18:03:41 -0300

    Workers

---
+++ work-queues/productor/src/main/java/main/TaskGenerator.java
@@ -0,0 +1,27 @@
+package main;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class TaskGenerator {
+    private static final String TASK_QUEUE_NAME = "dds_task_queue_1";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String[] messages = {"First message.", "Second message..", "Third message..."};
+
+            for(String message: messages) {
+                channel.basicPublish("", TASK_QUEUE_NAME,
+                        MessageProperties.PERSISTENT_TEXT_PLAIN,
+                        message.getBytes("UTF-8"));
+                System.out.println(" [x] Sent '" + message + "'");
+            }
+        }
+    }
+}
